bcff18d5524056ff572d5eab3a80bd0c71905340
smalancea <82141196+smalancea@users.noreply.github.com>
2021-06-16 12:19:12 +0300

    Import "admin" from "firebase-admin" in index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
 import { notifyAboutCommentHandler } from "./notifyAboutComment";
 import { createUserHandler } from "./createUser";
 import { getProfileHandler } from "./getProfile";
@@ -9,3 +10,5 @@ export const notifyAboutComment = functions.https.onRequest(
 
 export const createUser = functions.https.onRequest(createUserHandler);
 export const getProfile = functions.https.onRequest(getProfileHandler);
+
+admin.initializeApp(functions.config().firebase);
