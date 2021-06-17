cd60a0b3f6184a6e49ac736f6361f4854a7cf6b2
smalancea <82141196+smalancea@users.noreply.github.com>
2021-06-17 09:58:12 +0300

    Add remote schema

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin';
 import { notifyAboutCommentHandler } from "./notifyAboutComment";
 import { createUserHandler } from "./createUser";
 import { getProfileHandler } from "./getProfile";
+import initApolloServer from "./remoteSchema";
 
 export const notifyAboutComment = functions.https.onRequest(
     notifyAboutCommentHandler
@@ -10,5 +11,8 @@ export const notifyAboutComment = functions.https.onRequest(
 
 export const createUser = functions.https.onRequest(createUserHandler);
 export const getProfile = functions.https.onRequest(getProfileHandler);
+export const userProfile = functions.https.onRequest(
+    initApolloServer.createHandler()
+);
 
 admin.initializeApp(functions.config().firebase);
